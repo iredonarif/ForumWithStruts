4f9d808b66d22225afdc9f3d80202fcd58fcf08b
Arif <ogbonearif@gmail.com>
2019-06-21 16:42:37 +0000

    [Add] .gitignore file & update compile.sh

--- build/erreur.jsp
+++
@@ -1,8 +0,0 @@
-<%@ page contentType="text/html; charset=utf-8" %>
-<!DOCTYPE html>
-<html>
-	<head> <title>Authentification par formulaire </title> </head>
-	<body>
-		 <h2>Erreur d’authentification </h2>
-	</body>
-</html>
--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
-#JBOSS_HOME must have for value the path to wildfly folder
-export JBOSS_HOME=Wildfly_saved_folder/wildfly-15.0.1.Final
+#JBOSS_HOME must have for value the path to your web server folder
+export JBOSS_HOME=/path-to-your-web-server-folder
 ant archive
 cp dist/ForumWithStruts.war $JBOSS_HOME/standalone/deployments
